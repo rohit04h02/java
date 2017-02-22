0705c829465a4769403e5d60f8a18cfc53edbf54
rohit04h02 <rohit04h02@gmail.com>
2017-02-22 18:19:59 +0530

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.net.*;
+
+public class Client 
+	{
+	Socket s;
+	DataInputStream in;
+	DataOutputStream out;
+	Client()
+	{
+    try{
+	s=new Socket("localhost",10); //enter your ipv4 address
+	System.out.println("Client Started"+s);
+	System.out.println("Client Connected");
+	in=new DataInputStream(s.getInputStream());
+	out=new DataOutputStream(s.getOutputStream());
+	clientchat();
+	}
+	catch(Exception e)
+	{
+		System.out.println(e);		
+	}
+	}
+	public void clientchat()throws IOException
+	{
+	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));	
+   	String st;	
+    do
+	{
+	st=br.readLine();
+	out.writeUTF(st);
+    System.out.println("Server message :"+in.readUTF());
+	}while(!st.equals("stop"));	
+	}
+	public static void main(String s1[])
+	{
+		new Client();
+	}
+	}
+	
